99d14b446535ff478efc4fa7c852cce06029ddcd
Alireza <Alireza@AlirezaRezaei>
2018-07-18 22:10:54 +0200

    GitHub

---
+++ DroneDelivery/test/ueb02/DataTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ueb02;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+//import static org.testng.Assert.assertEquals;
+//import static org.testng.Assert.assertTrue;
+
+/**
+ *
+ * @author ite102770
+ */
+public class DataTest {
+    
+    public DataTest() {
+    }
+
+    @Test
+    public void testGetORderSeries_trueCase() {      
+        assertEquals(3, Data.getOrderSeries(1).length);
+        assertEquals(1, Data.getOrderSeries(3).length);
+        assertEquals(5, Data.getOrderSeries(2)[0][3]);
+    }
+     @Test
+    public void testGetORderSeries_invalidIndex() {      
+        assertEquals(null, Data.getOrderSeries(-1));
+       assertEquals(null, Data.getOrderSeries(4));
+    }
+    /***********************************************/
+     @Test
+    public void testGetMAP() {      
+        assertEquals(3, Data.getMap()[0][0][3]);
+       assertEquals(4, Data.getMap()[0][0][4]);
+       assertEquals(10, Data.getMap().length);
+       assertEquals(7, Data.getMap()[5].length);
+    }
+     @Test
+    public void testIsWarehouse() {      
+        assertTrue(Data.isWarehouse(0, 3));
+    }
+}
